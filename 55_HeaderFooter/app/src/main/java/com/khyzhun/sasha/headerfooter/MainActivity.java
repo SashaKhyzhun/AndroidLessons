c3715ad5552eb0032e4d362c5d55328ba27236f2
SashaKhyzhun <sasha6427@gmail.com>
2015-07-27 18:49:33 +0300

    1

--- 55_HeaderFooter/app/src/main/java/com/khyzhun/sasha/headerfooter/MainActivity.java
+++ 55_HeaderFooter/app/src/main/java/com/khyzhun/sasha/headerfooter/MainActivity.java
@@ -1,38 +1,65 @@
 package com.khyzhun.sasha.headerfooter;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    String[] data = {"one", "two", "three", "four", "five"};
+    ListView lvMain;
+    ArrayAdapter<String> adapter;
+
+    View header1;
+    View header2;
+
+    View footer1;
+    View footer2;
 
 
-public class MainActivity extends ActionBarActivity {
-
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvMain = (ListView) findViewById(R.id.lvMain);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+
+        // создаем Header и Footer
+        header1 = createHeader("header 1");
+        header2 = createHeader("header 2");
+        footer1 = createFooter("footer 1");
+        footer2 = createFooter("footer 2");
+
+        fillList();
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    // формирование списка
+    void fillList() {
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    // нажатие кнопки
+    public void onclick(View v) {
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    // создание Header
+    View createHeader(String text) {
+        View v = getLayoutInflater().inflate(R.layout.header, null);
+        ((TextView)v.findViewById(R.id.tvText)).setText(text);
+        return v;
+    }
 
-        return super.onOptionsItemSelected(item);
+    // создание Footer
+    View createFooter(String text) {
+        View v = getLayoutInflater().inflate(R.layout.footer, null);
+        ((TextView)v.findViewById(R.id.tvText)).setText(text);
+        return v;
     }
 }
