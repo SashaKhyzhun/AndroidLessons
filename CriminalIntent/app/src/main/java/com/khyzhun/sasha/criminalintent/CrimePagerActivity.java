179bf6a664d4b573579db18f624124583e2ce6a2
SashaKhyzhun <sasha6427@gmail.com>
2015-08-15 16:57:42 +0300

    ○

--- CriminalIntent/app/src/main/java/com/khyzhun/sasha/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/com/khyzhun/sasha/criminalintent/CrimePagerActivity.java
@@ -42,6 +42,26 @@ public class CrimePagerActivity extends FragmentActivity {
             }
         });
 
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int pos) {
+                Crime crime = mCrimes.get(pos);
+                if (crime.getTitle() != null) {
+                    setTitle(crime.getTitle());
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
         UUID crimeId = (UUID)getIntent()
                 .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
         for (int i = 0; i < mCrimes.size(); i++) {
