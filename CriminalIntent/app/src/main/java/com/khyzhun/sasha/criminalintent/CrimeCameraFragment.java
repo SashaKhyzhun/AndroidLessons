aa8c28725637c846e536d970bcba8b8b18252965
SashaKhyzhun <sasha6427@gmail.com>
2015-08-24 20:18:20 +0300

    7.6

--- CriminalIntent/app/src/main/java/com/khyzhun/sasha/criminalintent/CrimeCameraFragment.java
+++ CriminalIntent/app/src/main/java/com/khyzhun/sasha/criminalintent/CrimeCameraFragment.java
@@ -5,12 +5,17 @@ import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.util.Size;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Sasha on 24.08.15.
@@ -43,8 +48,70 @@ public class CrimeCameraFragment extends Fragment {
         // Camera работала на устройствах до 3.0
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                // Приказываем камере использовать указанную
+                // поверхность как область предварительного просмотра
+                try {
+                    if (mCamera != null) {
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception);
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                // Дальнейший вывод на поверхности невозможен,
+                // прекращаем предварительный просмотр.
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+
+                if (mCamera == null) return;
+
+                // Размер поверхности изменился; обновить размер
+                // области предварительноо просмотра камеры
+                Camera.Parameters parameters = mCamera.getParameters();
+                Size s = null; // TODO: 24.08.15 изменить!
+                parameters.setPreviewSize(s.getWidth(), s.getHeight()); // TODO: 24.08.15 -get
+                mCamera.setParameters(parameters);
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+        });
+
         return v;
     }
+
+
+
+    private Size getBestSupportedSize(List<Size> sizes, int width, int height) {
+        // Алгоритм получения найбольшего доступного размера
+        Size bestSize = sizes.get(0);
+        int largestArea = bestSize.getWidth() * bestSize.getHeight();
+        for (Size s : sizes) {
+            int area = s.getWidth() * s.getHeight();
+            if (area > largestArea) {
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+        return bestSize;
+
+    }
+
 
 
     @TargetApi(9)
