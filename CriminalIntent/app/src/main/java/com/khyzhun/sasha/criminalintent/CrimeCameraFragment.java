4a8f3a4aa480247ca7c952303a74678d0f84accd
SashaKhyzhun <sasha6427@gmail.com>
2015-08-28 16:01:48 +0300

    bad notebook
    reopen project, be care with updates, try to backup if need

--- CriminalIntent/app/src/main/java/com/khyzhun/sasha/criminalintent/CrimeCameraFragment.java
+++ CriminalIntent/app/src/main/java/com/khyzhun/sasha/criminalintent/CrimeCameraFragment.java
@@ -2,138 +2,144 @@ package com.khyzhun.sasha.criminalintent;
 
 import android.annotation.TargetApi;
 import android.hardware.Camera;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.util.Size;
-import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.Button;
-
 import java.io.IOException;
 import java.util.List;
 
-/**
- * Created by Sasha on 24.08.15.
- */
 public class CrimeCameraFragment extends Fragment {
 
     private static final String TAG = "CrimeCameraFragment";
-    private Camera mCamera;
-    private SurfaceView mSurfaceView;
 
+    private Camera camera;
+    private SurfaceView surfaceView;
 
     @Override
-    @SuppressWarnings("deprecation")
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
-        Button takePictureButton = (Button)v.findViewById(R.id.crime_camera_takePictureButton);
-        takePictureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().finish();
-            }
-        });
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
+        wireTakePictureButton(view);
 
-        mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
-        SurfaceHolder holder = mSurfaceView.getHolder();
-        // Метод setType() и константа SURFACE_TYPE_PUSH_BUFFERS считаются
-        // устаревшими, но они необходимы для того, чтобы функция
-        // предварительного просмотра изображения с камеры
-        // Camera работала на устройствах до 3.0
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceView = (SurfaceView)view.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = surfaceView.getHolder();
+        //Required for pre-Honeycomb compatibility.
+        setSurfaceHolderType(holder);
 
-        holder.addCallback(new SurfaceHolder.Callback() {
-            @Override
+        holder.addCallback(createSurfaceHolderCallback());
+
+        return view;
+    }
+
+    private SurfaceHolder.Callback createSurfaceHolderCallback() {
+        return new SurfaceHolder.Callback() {
             public void surfaceCreated(SurfaceHolder holder) {
-                // Приказываем камере использовать указанную
-                // поверхность как область предварительного просмотра
                 try {
-                    if (mCamera != null) {
-                        mCamera.setPreviewDisplay(holder);
+                    if (camera == null) {
+                        return;
                     }
-                } catch (IOException exception) {
-                    Log.e(TAG, "Error setting up preview display", exception);
+
+                    camera.setPreviewDisplay(holder);
+                } catch(IOException ex) {
+                    camera.release();
+                    Log.e(TAG, "Error setting up preview display", ex);
                 }
             }
 
-            @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
-                // Дальнейший вывод на поверхности невозможен,
-                // прекращаем предварительный просмотр.
-                if (mCamera != null) {
-                    mCamera.stopPreview();
+                if (camera == null) {
+                    return;
                 }
+
+                camera.stopPreview();
             }
 
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                if (camera == null) {
+                    return;
+                }
 
-                if (mCamera == null) return;
+                setPreviewSize();
 
-                // Размер поверхности изменился; обновить размер
-                // области предварительноо просмотра камеры
-                Camera.Parameters parameters = mCamera.getParameters();
-                Size s = null; // TODO: 24.08.15 изменить!
-                parameters.setPreviewSize(s.getWidth(), s.getHeight()); // TODO: 24.08.15 -get
-                mCamera.setParameters(parameters);
                 try {
-                    mCamera.startPreview();
-                } catch (Exception e) {
-                    Log.e(TAG, "Could not start preview", e);
-                    mCamera.release();
-                    mCamera = null;
+                    camera.startPreview();
+                } catch (Exception ex) {
+                    Log.e(TAG, "Could not start preview", ex);
+                    camera.release();
+                    camera = null;
                 }
             }
-        });
 
-        return v;
-    }
-
-
-
-    private Size getBestSupportedSize(List<Size> sizes, int width, int height) {
-        // Алгоритм получения найбольшего доступного размера
-        Size bestSize = sizes.get(0);
-        int largestArea = bestSize.getWidth() * bestSize.getHeight();
-        for (Size s : sizes) {
-            int area = s.getWidth() * s.getHeight();
-            if (area > largestArea) {
-                bestSize = s;
-                largestArea = area;
+            private void setPreviewSize() {
+                Camera.Parameters parameters = camera.getParameters();
+                Camera.Size size = getBestSupportedSize(parameters.getSupportedPreviewSizes());
+                parameters.setPreviewSize(size.width, size.height);
+                camera.setParameters(parameters);
             }
-        }
-        return bestSize;
 
+            private Camera.Size getBestSupportedSize(List<Camera.Size> sizes) {
+                Camera.Size bestSize = sizes.get(0);
+
+                int largestArea = bestSize.width * bestSize.height;
+                for (Camera.Size s : sizes) {
+                    int area = s.width * s.height;
+                    if (area > largestArea) {
+                        bestSize = s;
+                        largestArea = area;
+                    }
+                }
+
+                return bestSize;
+            }
+        };
     }
 
-
+    /**
+     * Required for pre-Honeycomb compatibility.
+     */
+    @SuppressWarnings("deprecation")
+    private void setSurfaceHolderType(SurfaceHolder holder) {
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
 
     @TargetApi(9)
     @Override
     public void onResume() {
         super.onResume();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-            mCamera = Camera.open(0);
-        } else {
-            mCamera = Camera.open();
-        }
+        //openCamera();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if(mCamera != null) {
-            mCamera.release();
-            mCamera = null;
+        releaseCamera();
+    }
+
+    private void releaseCamera() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
         }
     }
 
+    /*private void openCamera() {
+        if (AndroidVersionHelper.isGingerbreadOrHigher()) {
+            camera = Camera.open(0);
+        } else {
+            camera = Camera.open();
+        }
+    }*/
+
+
+    private void wireTakePictureButton(View view) {
+        Button takePictureButton = (Button)view.findViewById(R.id.crime_camera_takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+    }
 
 
 }
