97161c5e56e9ac7e17375322d408e30a65da2087
SashaKhyzhun <sasha6427@gmail.com>
2015-09-08 00:41:29 +0300

    application for site Flickr, just connect and watch pict. ez

---
+++ PhotoGallery/app/src/main/java/sasha/khyzhun/com/photogallery/GalleryItem.java
@@ -0,0 +1,44 @@
+package sasha.khyzhun.com.photogallery;
+
+/**
+ * Created by Dima on 07-Sep-15.
+ */
+public class GalleryItem {
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+
+    public String toString() {
+        return mCaption;
+    }
+
+
+    /** List of Getters **/
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    /** List of Setters **/
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+}
---
+++ PhotoGallery/app/src/main/java/sasha/khyzhun/com/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package sasha.khyzhun.com.photogallery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Sasha on 12.08.15.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fragManager = getSupportFragmentManager();
+        Fragment fragment = fragManager.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = createFragment();
+            fragManager.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+}
