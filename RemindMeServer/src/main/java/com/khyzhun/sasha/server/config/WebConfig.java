6bd107b53be9bbc9ab68c4d02a40e8d1b24d1320
SashaKhyzhun <sasha6427@gmail.com>
2016-01-03 23:08:14 +0200

    server done, SQL is comming

---
+++ RemindMeServer/src/main/java/com/khyzhun/sasha/server/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.khyzhun.sasha.server.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.khyzhun.sasha.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
