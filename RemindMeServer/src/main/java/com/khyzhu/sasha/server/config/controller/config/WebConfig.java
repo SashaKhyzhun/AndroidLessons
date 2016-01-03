e87c3d7b302f7eb2f55e29a3be08bd231fa957b3
SashaKhyzhun <sasha6427@gmail.com>
2016-01-03 20:07:12 +0200

    servlet + dependency. bcp

---
+++ RemindMeServer/src/main/java/com/khyzhu/sasha/server/config/controller/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.khyzhu.sasha.server.config.controller.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
